4bca294163e20fb916ef16a86f3465ad0b2df958
Alexey Egorov <lifeisgoodvega@gmail.com>
2020-09-26 20:10:51 +0300

    main task

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -5,9 +5,10 @@ echo "MongoDB installation: Add repo to sources list"
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 echo "MongoDB installation: Installing https support"
 apt-get -y install apt-transport-https ca-certificates
+sleep 2
 echo "MongoDB installation: Updating apt"
 apt-get update
-sleep 1
+sleep 2
 echo "MongoDB installation: Installing mongodb package"
 apt-get install -y mongodb-org
 echo "MongoDB installation: Start mongodb"
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "external_ip_address_app" {
-  value = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
-}
-
-output "external_ip_address_lb" {
-  value = element(element(yandex_lb_network_load_balancer.lb.listener.*, 0).external_address_spec.*.address, 0)
-}
