3d57ed3e3caeaa3764f0af230169a76f6d4f2f8b
Alexey Egorov <lifeisgoodvega@gmail.com>
2020-10-03 23:34:15 +0300

    travis passage tweaks

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -4,6 +4,14 @@ variable cloud_id {
 variable folder_id {
   description = "Folder"
 }
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-b"
+}
+variable instance_zone {
+  description = "Zone of VM instance"
+  default     = "ru-central1-b"
+}
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
@@ -16,8 +24,16 @@ variable image_id {
 variable subnet_id {
   description = "Subnet"
 }
+variable instances_count {
+  description = "How much of instances will work under load balancer"
+  default     = 1
+}
 variable service_account_key_file {
   description = "key .json"
+}
+variable reddit_listener_port {
+  description = "reddit listener port"
+  default     = 9292
 }
 variable app_disk_image {
   description = "Disk image for reddit app"
